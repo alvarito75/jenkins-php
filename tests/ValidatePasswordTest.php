9e90cc91d665b95396e0b0cad74ac91542a4b793
Alvarito75 <alvarito75@outlook.com>
2021-11-22 19:49:18 -0400

    Password validation PHP Unit test, in Jenkins

--- tests/ValidatePasswordTest.php
+++ tests/ValidatePasswordTest.php
@@ -8,6 +8,6 @@ class ValidatePasswordTest extends TestCase {
 
     public function testValidateLength() {
         $validatePass = new ValidatePassword();
-        $this->assertFalse($validatePass->validateLength('12345'));
+        $this->assertFalse($validatePass->validateLength('123456'));
     }
 }
